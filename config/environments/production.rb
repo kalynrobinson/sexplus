c3dede48dc846457a860af1277bb9cf29ec72882
kalynrobinson <kalynrobinson@gmail.com>
2017-04-02 06:41:16 -0400

    Mailer bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.action_mailer.default_url_options = { host: 'fierce-springs-72446.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
